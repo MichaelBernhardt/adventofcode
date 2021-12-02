31151cbe0be240ec5249a1ffdbbd66c53a763bed
MichaelBernhardt <earthlingmikey@gmail.com>
2021-12-02 13:01:51 +0200

    Added Dec 2 task 2

---
+++ 2 Dec/task2.js
@@ -0,0 +1,34 @@
+let fs = require('fs');
+let array = fs.readFileSync('input.txt').toString().split("\n");
+
+let depth = 0;
+let horizontal = 0;
+let aim = 0;
+let instuctionValue = 0;
+
+for (let i = 0; i < array.length; i++) {
+    
+    instuctionValue = parseInt((array[i].match(/\d+/)));
+
+    if (array[i].includes("forward")) {
+        horizontal = horizontal + instuctionValue;
+        depth = depth + (aim * instuctionValue);
+    }
+
+    if (array[i].includes("down")) {
+        aim = aim + instuctionValue;
+    }
+
+    if (array[i].includes("up")){
+        aim = aim - instuctionValue;
+    }
+}
+
+const finalAnswer = depth * horizontal;
+
+console.log("Horizontal: " + horizontal);
+console.log("Depth: " + depth);
+console.log("Aim: " + aim);
+console.log("Final answer. Horizontal x Depth = " + finalAnswer);
+
+
